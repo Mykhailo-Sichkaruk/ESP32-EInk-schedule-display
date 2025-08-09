d4d5bba41b38e49e3791d06ab28385140661fe24
Illia Chaban <ic-it@mail.com>
2025-08-09 23:03:23 +0200

    РАБОТАЕТ

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,54 @@ use embedded_graphics::prelude::Size;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::PrimitiveStyleBuilder;
 use embedded_graphics::primitives::Rectangle;
-use epd_waveshare::color::Color;
-use epd_waveshare::epd2in9::Display2in9;
-use epd_waveshare::epd2in9::Epd2in9;
+// use epd_waveshare::color::Color;
+use epd_waveshare::color::TriColor;
+use epd_waveshare::epd7in5b_v3::Display7in5;
+use epd_waveshare::epd7in5b_v3::Epd7in5;
 use epd_waveshare::prelude::WaveshareDisplay;
 use esp_idf_hal::delay::Delay;
 use esp_idf_hal::gpio;
-use esp_idf_hal::gpio::*;
+use esp_idf_hal::gpio::AnyInputPin;
+use esp_idf_hal::gpio::AnyOutputPin;
+use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::spi;
 use esp_idf_sys::esp_deep_sleep_start;
-use esp_idf_sys::esp_deep_sleep_try;
-use esp_idf_sys::esp_deep_sleep_try_to_start;
 use esp_idf_sys::esp_sleep_enable_ext0_wakeup;
-use esp_idf_sys::esp_sleep_enable_gpio_wakeup;
-use esp_idf_sys::esp_sleep_enable_timer_wakeup;
 use log::info;
 
 fn main() -> anyhow::Result<()> {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
-    // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().expect("Failed to take peripherals");
+    info!("!!!!!!!!!!!!!!!!!!!!Starting EPD example");
+
+    let peripherals = {
+        let this = Peripherals::take();
+        match this {
+            Ok(t) => t,
+            Err(e) => {
+                info!("!!!!!!!!!!!!!!!!!!!!Failed to take peripherals: {e}");
+                panic!("@@@@@@@@@@@@@@@@@@@@Failed to take peripherals");
+            }
+        }
+    };
 
     let spi = peripherals.spi3;
     let sclk = peripherals.pins.gpio18;
-    let mosi = peripherals.pins.gpio19;
+    let mosi = peripherals.pins.gpio23;
     let cs = peripherals.pins.gpio5;
 
     let busy_in: AnyInputPin = peripherals.pins.gpio4.into();
-    let rst: AnyOutputPin = peripherals.pins.gpio21.into();
-    let dc: AnyOutputPin = peripherals.pins.gpio23.into();
+    let rst: AnyOutputPin = peripherals.pins.gpio16.into();
+    let dc: AnyOutputPin = peripherals.pins.gpio17.into();
+
+    let pwr: AnyOutputPin = peripherals.pins.gpio2.into();
+    let mut pwr = PinDriver::output(pwr)?;
+    pwr.set_high()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(1000));
 
     let mut spidd = spi::SpiDeviceDriver::new_single(
         spi,
@@ -47,66 +60,149 @@ fn main() -> anyhow::Result<()> {
         Option::<gpio::AnyIOPin>::None,
         Some(cs),
         &spi::config::DriverConfig::new(),
-        &spi::config::Config::new().baudrate(115200.Hz().into()),
+        &spi::config::Config::new().baudrate(115200.Hz()),
     )?;
-    info!("SPI2 driver setup completed");
+    info!("!!!!!!!!!!!!!!!!!!!!SPI2 driver setup completed");
+    std::thread::sleep(std::time::Duration::from_millis(1000));
 
     let mut delay = Delay::new(100);
 
-    let mut display = Display2in9::default();
-    let mut epd = Epd2in9::new(
-        &mut spidd,
-        PinDriver::input(busy_in)?,
-        PinDriver::output(dc)?,
-        PinDriver::output(rst)?,
-        &mut delay,
-        100.into(),
-    )
-    .expect("Failed to create Epd7in5 driver");
+    let mut display = Display7in5::default();
+    let mut epd = {
+        let this = Epd7in5::new(
+            &mut spidd,
+            PinDriver::input(busy_in)?,
+            PinDriver::output(dc)?,
+            PinDriver::output(rst)?,
+            &mut delay,
+            100.into(),
+        );
+        match this {
+            Ok(t) => t,
+            Err(e) => {
+                info!("!!!!!!!!!!!!!!!!!!!!Failed to create Epd7in5 driver: {e}");
+                panic!("@@@@@@@@@@@@@@@@@@@@Failed to create Epd7in5 driver");
+            }
+        }
+    };
+    //
 
-    info!("Drawing completed");
+    info!("!!!!!!!!!!!!!!!!!!!!Drawing completed");
+    std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    epd.wake_up(&mut spidd, &mut delay)
-        .expect("Failed to wake up EPD");
-    info!("EPD wake up completed");
+    {
+        let this = epd.wake_up(&mut spidd, &mut delay);
+        match this {
+            Ok(t) => t,
+            Err(e) => {
+                info!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Failed to wake up EPD: {e}");
+                panic!("@@@@@@@@@@@@@@@@@@@@Failed to wake up EPD");
+            }
+        }
+    };
+    info!("!!!!!!!!!!!!!!!!!!!!EPD wake up completed");
+    std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    epd.clear_frame(&mut spidd, &mut delay)
-        .expect("Failed to wake up EPD");
-    info!("EPD frame cleared");
+    {
+        let this = epd.clear_frame(&mut spidd, &mut delay);
+        match this {
+            Ok(t) => t,
+            Err(e) => {
+                info!("!!!!!!!!!!!!!!!!!!!!Failed to clear EPD frame: {e}");
+                panic!("@@@@@@@@@@@@@@@@@@@@Failed to clear EPD frame");
+            }
+        }
+    };
+    info!("!!!!!!!!!!!!!!!!!!!!EPD frame cleared");
+    std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    display
-        .clear(Color::Black)
-        .expect("Failed to clear display");
-    info!("Display cleared");
+    // {
+    //     let this = display.clear(TriColor::Black);
+    //     match this {
+    //         Ok(t) => t,
+    //         Err(e) => {
+    //             info!("!!!!!!!!!!!!!!!!!!!!Failed to clear display: {e}");
+    //             panic!("@@@@@@@@@@@@@@@@@@@@Failed to clear display");
+    //         }
+    //     }
+    // };
+    // info!("!!!!!!!!!!!!!!!!!!!!Display cleared");
+    // std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    let style = PrimitiveStyleBuilder::new()
-        .fill_color(Color::White)
-        .stroke_width(0)
-        .build();
-    Rectangle::new(Point::new(10, 10), Size::new(60, 40))
-        .into_styled(style)
-        .draw(&mut display)
-        .expect("Failed to draw rectangle");
-    info!("Rectangle drawn");
+    // let style = PrimitiveStyleBuilder::new()
+    //     .fill_color(TriColor::White)
+    //     .stroke_width(0)
+    //     .build();
+    // {
+    //     let this = Rectangle::new(Point::new(10, 10), Size::new(60, 40))
+    //         .into_styled(style)
+    //         .draw(&mut display);
+    //     match this {
+    //         Ok(t) => t,
+    //         Err(e) => {
+    //             info!("!!!!!!!!!!!!!!!!!!!!Failed to draw rectangle: {e}");
+    //             panic!("@@@@@@@@@@@@@@@@@@@@Failed to draw rectangle");
+    //         }
+    //     }
+    // };
+    // info!("!!!!!!!!!!!!!!!!!!!!Rectangle drawn");
+    // std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    epd.update_frame(&mut spidd, display.buffer(), &mut delay)
-        .expect("Failed to update frame");
-    info!("EPD frame updated with rectangle");
+    // {
+    //     let this = epd.update_frame(&mut spidd, display.buffer(), &mut delay);
+    //     match this {
+    //         Ok(t) => t,
+    //         Err(e) => {
+    //             info!("!!!!!!!!!!!!!!!!!!!!Failed to update frame: {e}");
+    //             panic!("@@@@@@@@@@@@@@@@@@@@Failed to update frame");
+    //         }
+    //     }
+    // };
+    // info!("!!!!!!!!!!!!!!!!!!!!EPD frame updated with rectangle");
+    // std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    epd.display_frame(&mut spidd, &mut delay)
-        .expect("Failed to display frame");
+    // {
+    //     let this = epd.display_frame(&mut spidd, &mut delay);
+    //     match this {
+    //         Ok(t) => t,
+    //         Err(e) => {
+    //             info!("!!!!!!!!!!!!!!!!!!!!Failed to display frame: {e}");
+    //             panic!("@@@@@@@@@@@@@@@@@@@@Failed to display frame");
+    //         }
+    //     }
+    // };
 
-    epd.sleep(&mut spidd, &mut delay)
-        .expect("Failed to put EPD to sleep");
+    // {
+    //     let this = epd.sleep(&mut spidd, &mut delay);
+    //     match this {
+    //         Ok(t) => t,
+    //         Err(e) => {
+    //             info!("!!!!!!!!!!!!!!!!!!!!Failed to put EPD to sleep: {e}");
+    //             panic!("@@@@@@@@@@@@@@@@@@@@Failed to put EPD to sleep");
+    //         }
+    //     }
+    // };
 
-    info!("EPD put to sleep");
+    // info!("!!!!!!!!!!!!!!!!!!!!EPD put to sleep");
+    // std::thread::sleep(std::time::Duration::from_millis(1000));
 
-    let wakeup_pin = PinDriver::input(peripherals.pins.gpio33).expect("wakeup pin sleep");
-    unsafe { esp_idf_sys::esp_sleep_enable_ext0_wakeup(wakeup_pin.pin(), 0); }
-    unsafe {
-        esp_sleep_enable_ext0_wakeup(25, 1);
-        esp_deep_sleep_start();
-    }
+    // // let wakeup_pin = {
+    // //     let this = PinDriver::input(peripherals.pins.gpio33);
+    // //     match this {
+    // //         Ok(t) => t,
+    // //         Err(e) => {
+    // //             info!("!!!!!!!!!!!!!!!!!!!!Failed to create wakeup pin: {e}");
+    // //             panic!("@@@@@@@@@@@@@@@@@@@@Failed to create wakeup pin");
+    // //         }
+    // //     }
+    // // };
+    // // unsafe {
+    // //     esp_idf_sys::esp_sleep_enable_ext0_wakeup(wakeup_pin.pin(), 0);
+    // // }
+    // // unsafe {
+    // //     esp_sleep_enable_ext0_wakeup(25, 1);
+    // //     esp_deep_sleep_start();
+    // // }
 
     Ok(())
 }
